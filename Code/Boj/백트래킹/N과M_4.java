371d229b889c25bf6f41d8ca6c38abd455e76063
tilsong <cnsong0229@gmail.com>
2023-03-10 13:54:19 +0900

    add 230310 Boj 백트래킹 - N과 M 4

---
+++ Code/Boj/백트래킹/N과M_4.java
@@ -0,0 +1,53 @@
+package Boj.백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 10m (Success)
+// 입력: 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+// 출력: 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+//		수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+// 풀이 방향: 백트래킹 사용. 각 depth 별로 별도의 방문처리 배열을 만들어 사용했음.
+// 시간 복잡도: O(n^m) => 최대 8^8
+public class N과M_4 {
+	static int n;
+	static int m;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken()); // depth
+		arr = new int[m];
+
+		search(0,0);
+
+		System.out.println(sb.toString());
+	}
+
+	static void search(int depth, int before) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+
+			return;
+		}
+
+		boolean[] visited = new boolean[n+1];
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i] && before <= i) {
+				visited[i] = true;
+				arr[depth] = i;
+				search(depth+1, i);
+				visited[i] = false;
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -119,6 +119,7 @@
 - [230302 | N과 M(1)(B15649)](https://github.com/tilsong/TodayAlgorithm/blob/main/Code/Boj/백트래킹/N과M_1.java)
 - [230310 | N과 M(2)(B15650)](https://github.com/tilsong/TodayAlgorithm/blob/main/Code/Boj/백트래킹/N과M_2.java)
 - [230310 | N과 M(3)(B15651)](https://github.com/tilsong/TodayAlgorithm/blob/main/Code/Boj/백트래킹/N과M_3.java)
+- [230310 | N과 M(4)(B15652)](https://github.com/tilsong/TodayAlgorithm/blob/main/Code/Boj/백트래킹/N과M_4.java)
 
 ### 투포인터
 - [230214 | 두 수의 합(B9024)](https://github.com/tilsong/TodayAlgorithm/blob/main/Code/Boj/투포인터/두수의합.java)
