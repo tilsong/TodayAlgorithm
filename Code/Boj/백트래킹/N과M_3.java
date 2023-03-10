2091dbd193f8deccf5b10f68bf3026b7a0fe18f8
tilsong <cnsong0229@gmail.com>
2023-03-10 13:46:18 +0900

    add 230310 Boj 백트래킹 - N과 M 3

--- Code/Boj/백트래킹/N과M_3.java
+++ Code/Boj/백트래킹/N과M_3.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 40m (Success)
-// 입력: 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+// 25m (Success)
+// 입력: 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
 // 출력: 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
 //		수열은 사전 순으로 증가하는 순서로 출력해야 한다.
-// 풀이 방향: 백트래킹 사용. 이전 수열의 값보다 현재 값이 커야한다는 조건을 추가함
-// 시간 복잡도: O(n^m) => 최대 8^8
-
-public class N과M_2 {
+// 풀이 방향: 백트래킹 사용. 각 depth 별로 별도의 방문처리 배열을 만들어 사용했음.
+// 시간 복잡도: O(n^m) => 최대 7^7
+public class N과M_3 {
 	static int n;
 	static int m;
-	static boolean[] visited;
 	static int[] arr;
 	static StringBuilder sb = new StringBuilder();
 
@@ -25,11 +23,9 @@ public class N과M_2 {
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken()); // depth
+		arr = new int [m];
 
-		visited = new boolean[n+1];
-		arr = new int[m];
-
-		search(0, 0);
+		search(0,0);
 
 		System.out.println(sb.toString());
 	}
@@ -39,18 +35,21 @@ public class N과M_2 {
 			for (int i = 0; i < m; i++) {
 				sb.append(arr[i]).append(' ');
 			}
-			sb.append("\n");
+			sb.append('\n');
 
 			return;
 		}
 
+		boolean[] visited = new boolean[n+1];
+
 		for (int i = 1; i <= n; i++) {
-			if (!visited[i] && before < i) {
+			if (!visited[i]) {
 				visited[i] = true;
 				arr[depth] = i;
 				search(depth+1, i);
 				visited[i] = false;
 			}
 		}
+
 	}
 }
