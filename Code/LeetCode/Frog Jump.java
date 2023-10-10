7716687d4f55bafb214032994ae92c9bf1b51014
Eunseok Song <cnsong0229@gmail.com>
2023-10-11 01:34:52 +0900

    add 231010 릿코드 - Frog Jump

---
+++ Code/LeetCode/Frog Jump.java
@@ -0,0 +1,28 @@
+// 풀이 링크 https://www.notion.so/song229/Frog-Jump-14b15151246847deb8008c41b5f40f26
+
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+        boolean[][] dp = new boolean[n][n];
+        dp[0][0] = true;
+
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<i; j++) {
+                int diff = stones[i] - stones[j];
+                if (diff>=n) {
+                    continue;
+                }
+                if ((diff-1>=0 && dp[j][diff-1]) || dp[j][diff] || (diff+1<n && dp[j][diff+1])) { 
+                    dp[i][diff] = true;
+                }
+            }
+        }
+
+        for (int j=0; j<n; j++) {
+            if (dp[n-1][j]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
